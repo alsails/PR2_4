e6097a01539c57000407b64ec89f146d2150c267
alsails <mash.pogonina2014@yandex.ru>
2025-03-19 22:20:07 +0300

    Изменение упражнение 1

--- PR2/Task 1/task1_2.py
+++ PR2/Task 1/task1_2.py
@@ -4,79 +4,84 @@ pygame.init()
 
 screen = pygame.display.set_mode((800, 600), pygame.RESIZABLE)
 pygame.display.set_caption("Погонина М.В. Упражнение 1")
-screen.fill('white')
-
-width, height = screen.get_size()
-center = (width // 2, height // 2)
 
 
-# Функция отрисовки домика
-def draw_house():
-    # Домик
+def draw_house(screen, width, height):
+    screen.fill('white')
+
+    center = (width // 2, height // 2)
+
+    top_peak = center
+
+    # Размеры дома
     house_width, house_height = 300, 200
-    house_x = center[0] - house_width // 2
-    house_y = center[1] - house_height // 2
+    roof_height = 120
 
-    pygame.draw.rect(screen, (60, 179, 113),
-                     (house_x, house_y, house_width, house_height))  # корпус
+    bottom_offset = height - 20 - (top_peak[1] + roof_height + house_height)
 
-    # Крыша
-    left_corner = (house_x, house_y)
-    right_corner = (house_x + house_width, house_y)
-    top_peak = (house_x + house_width // 2, house_y - 120)
-    pygame.draw.polygon(screen, (83, 128, 134),
-                        [left_corner, right_corner, top_peak])  # крыша
+    # Координаты углов
+    left_corner = (top_peak[0] - house_width // 2,
+                   top_peak[1] + roof_height + bottom_offset)
+    right_corner = (top_peak[0] + house_width // 2,
+                    top_peak[1] + roof_height + bottom_offset)
+    bottom_left = (left_corner[0], left_corner[1] + house_height)
+    bottom_right = (right_corner[0], right_corner[1] + house_height)
 
-    # Дверь
+    # Линии крыши
+    pygame.draw.line(screen, (0, 0, 0), top_peak, left_corner, 3)
+    pygame.draw.line(screen, (0, 0, 0), top_peak, right_corner, 3)
+    pygame.draw.line(screen, (0, 0, 0), left_corner, right_corner, 3)
+
+    # Линии стен
+    pygame.draw.line(screen, (0, 0, 0), left_corner, bottom_left, 3)
+    pygame.draw.line(screen, (0, 0, 0), right_corner, bottom_right, 3)
+    pygame.draw.line(screen, (0, 0, 0), bottom_left, bottom_right, 3)
+
+    # Линии двери
     door_width, door_height = 90, 120
-    door_x = house_x + (house_width // 2) - (door_width // 2)
-    door_y = house_y + house_height - door_height  # по низу дома
-    pygame.draw.rect(screen, (165, 112, 58),
-                     (door_x, door_y, door_width, door_height))  # дверь
+    door_x = bottom_left[0] + (house_width // 2) - (door_width // 2)
+    door_y = bottom_left[1] - door_height
+    pygame.draw.line(screen, (0, 0, 0), (door_x, door_y),
+                     (door_x, bottom_left[1]), 3)
+    pygame.draw.line(screen, (0, 0, 0), (door_x + door_width,
+                     door_y), (door_x + door_width, bottom_left[1]), 3)
+    pygame.draw.line(screen, (0, 0, 0), (door_x, door_y),
+                     (door_x + door_width, door_y), 3)
 
-    # Окошко
-    roof_center_x = house_x + house_width // 2
-    roof_center_y = house_y - 40
-    window_radius = 25
-    pygame.draw.circle(screen, (255, 255, 224),
-                       (roof_center_x, roof_center_y), window_radius)  # окно
+    # Линии окна
+    window_size = 50
+    window_x = bottom_left[0] + (house_width // 4) - (window_size // 2)
+    window_y = bottom_left[1] - house_height // 2 - (window_size // 2)
+    pygame.draw.line(screen, (0, 0, 0), (window_x, window_y),
+                     (window_x + window_size, window_y), 3)
+    pygame.draw.line(screen, (0, 0, 0), (window_x, window_y + window_size),
+                     (window_x + window_size, window_y + window_size), 3)
+    pygame.draw.line(screen, (0, 0, 0), (window_x, window_y),
+                     (window_x, window_y + window_size), 3)
+    pygame.draw.line(screen, (0, 0, 0), (window_x + window_size, window_y),
+                     (window_x + window_size, window_y + window_size), 3)
 
-    # Перекладины
-    line_color = (0, 0, 0)
-    line_width = 3
-    pygame.draw.line(screen, line_color,
-                     (roof_center_x, roof_center_y - window_radius),
-                     (roof_center_x, roof_center_y + window_radius),
-                     line_width)
-    pygame.draw.line(screen, line_color,
-                     (roof_center_x - window_radius, roof_center_y),
-                     (roof_center_x + window_radius, roof_center_y),
-                     line_width)
+    # Перекладины окна
+    pygame.draw.line(screen, (0, 0, 0),
+                     (window_x, window_y + window_size // 2),
+                     (window_x + window_size, window_y + window_size // 2), 3)
+    pygame.draw.line(screen, (0, 0, 0),
+                     (window_x + window_size // 2, window_y),
+                     (window_x + window_size // 2, window_y + window_size), 3)
 
-    return house_x, house_y, house_width, house_height
-
-
-house_x, house_y, house_width, house_height = draw_house()
-
-desired_height = 150
-image = pygame.image.load('PR2/Task 1/grass.png')
-scaled_image = pygame.transform.scale(image, (width, desired_height))
-
-screen.blit(scaled_image, (0, house_y + house_height - 115))
-pygame.display.flip()
-
-# Задержка
-pygame.time.delay(1000)
-screen.fill('white')
-draw_house()
-screen.blit(scaled_image, (0, house_y + house_height - 50))
-
-pygame.display.flip()
 
 running = True
 while running:
+    width, height = screen.get_size()
+    draw_house(screen, width, height)  # Вызов функции для отрисовки домика
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.VIDEORESIZE:
+            screen = pygame.display.set_mode((event.w, event.h),
+                                             pygame.RESIZABLE)
+
+    pygame.display.flip()  # Обновление экрана
 
 pygame.quit()
